0faa1b514153713e022f3b51076dd9b203212021
countless-integers <totcha@gazeta.pl>
2018-08-16 09:05:38 +0200

    all-time classic

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzzCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100) {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                bool fizzbuzz = fizz && buzz;
+                if (fizzbuzz) {
+                    Console.WriteLine("FizzBuzz");
+                } else if (buzz) {
+                    Console.WriteLine("Buzz");
+                } else if (fizz) {
+                    Console.WriteLine("Fizz");
+                } else {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
